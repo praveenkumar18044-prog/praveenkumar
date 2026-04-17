0bf9cd769967ef904fcf1b501762537aa50c79d8
praveenkumar <praveenkumar18044@gmail.com>
2026-04-17 10:04:54 +0530

    task2

---
+++ TASK2/SQLalchemy/config.py
@@ -0,0 +1,5 @@
+class Config:
+    SECRET_KEY = 'dev_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root123@localhost/student_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False   
+    
